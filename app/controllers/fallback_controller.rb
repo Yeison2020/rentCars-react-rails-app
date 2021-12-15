05c7db973cd9d7ae83890b6e430d063d14273fd4
Yeison <enmanuelquiterios@gmail.com>
2021-12-15 14:13:25 -0500

    Remove some docs from React app

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -2,6 +2,6 @@ class FallbackController < ActionController::Base
 
     def index
         #I need to create a site here to render in case of not found page.
-        render json: {error: 'Page not found'},status: :not_found 
+        render json: {error: 'Page not found Please try checking your Link'},status: :not_found 
     end
 end
