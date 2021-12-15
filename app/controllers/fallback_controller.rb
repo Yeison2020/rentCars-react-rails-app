7dd21fdb0bc6249b634b4c62476f46bf132af933
Yeison <enmanuelquiterios@gmail.com>
2021-12-15 12:57:21 -0500

    Added Package.json with Heroku and React config

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,6 +1,7 @@
-class FallbackController < ApplicationController
+class FallbackController < < ActionController::Base
 
-    def index 
-
+    def index
+        #I need to create a site here to render in case of not found page.
+        render json: {error: 'Page not found'},status: :not_found 
     end
 end
